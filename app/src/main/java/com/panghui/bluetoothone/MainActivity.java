c2c387633419b21e93c20c54d7876ef4a0bb7be6
Peter <panghuinow@gmail.com>
2018-04-19 20:54:02 +0800

    locate successfully

--- app/src/main/java/com/panghui/bluetoothone/MainActivity.java
+++ app/src/main/java/com/panghui/bluetoothone/MainActivity.java
@@ -1,9 +1,17 @@
 package com.panghui.bluetoothone;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -12,11 +20,18 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.panghui.bluetoothone.base.AppConst;
 import com.panghui.bluetoothone.utils.HexUtils;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,AMapLocationListener{
+    /**蓝牙部分*/
     private BluetoothChatService mChatService;
 
     private ImageView connect;
@@ -28,23 +43,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private CheckBox cb_hex;
     private ScrollView mScrollView;
 
+    /**定位部分*/
+    //声明 AMapLocationClient 类对象
+    public AMapLocationClient mLocationClient=null;
+    //已实现回调监听器的接口
+
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption=null;
+    private Context mContext;
+    double mylatitude;
+    double mylongtitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mContext=getApplicationContext();
         initView();
-
         mChatService = new BluetoothChatService(mHandler);
+        /**注册权限*/
+        registerPermissions();
+        /**获取定位服务*/
+        location();
+
     }
+
+    /**定位*/
+    public void location(){
+        mLocationClient = new AMapLocationClient(mContext);
+        mLocationClient.setLocationListener(this);
+        mLocationOption=new AMapLocationClientOption();
+        //高精度定位模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //自定义连续定位
+        mLocationOption.setInterval(1000);
+        //不需要返回地址信息
+        mLocationOption.setNeedAddress(false);
+
+        mLocationClient.setLocationOption(mLocationOption);
+        mLocationClient.startLocation();
+    }
+
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mChatService.stop();
+        mLocationClient.stopLocation();
+        mLocationClient.onDestroy();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0){
+                    for (int result:grantResults){
+                        if (result!=PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须同意所有权限才能使用本程序",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    //TODO
+
+                }else{
+                    Toast.makeText(this, "发生未知错误！", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
     }
 
     /*********************function defien*********************************************/
+    private void registerPermissions(){
+        List<String> permissionList=new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()){
+            String[] permissions=permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }else{
+            //TODO
+
+        }
+    }
 
     private void initView(){
         connect=(ImageView)findViewById(R.id.im_connect);
@@ -148,4 +248,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     };
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation.getErrorCode()==0){
+            mylatitude=aMapLocation.getLatitude();
+            mylongtitude=aMapLocation.getLongitude();
+            String lat=Double.toString(mylatitude);
+            String lon=Double.toString(mylongtitude);
+            Toast.makeText(MainActivity.this,lat+"|"+lon,Toast.LENGTH_SHORT).show();
+        }else{
+            Log.e("AmapError","location Error, ErrorCode:"
+            +aMapLocation.getErrorCode()+",errorInfo:"+aMapLocation.getErrorInfo());
+        }
+    }
 }
